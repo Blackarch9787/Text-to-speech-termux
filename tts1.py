1f733784b787d8e170da5fb6922a1961f794902e
blackdevil <blackdevil@kali>
2024-08-24 19:21:16 +0530

    version 1.02

--- tts1.py
+++ tts1.py
@@ -1,19 +1,30 @@
 #import modules
 import subprocess
 import gtts
-import time
 #welcome tts msg
 welcomemsg="welcome to terminal voice assistant"
 a1=gtts.gTTS(welcomemsg)
 a1.save("tts1.mp3")
 subprocess.call("mpv tts1.mp3",shell=True)
-print("welcome to text to speech termux...")
+# store a color codes into class ...
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+print(bcolors.OKGREEN+"welcome to text to speech termux...")
+print(bcolors.OKGREEN+welcomemsg)
 
+
+
+#command program
 def repeat():
     msg=input("Enter A Command: ")
-    
-
-    
     match msg:
         case "clear":
             a=gtts.gTTS("clearing a terminal")
@@ -25,11 +36,13 @@ def repeat():
             a.save("tts.mp3")
             subprocess.call("mpv tts.mp3",shell=True)
             subprocess.call(msg,shell=True),
+            
         case "pwd":
             a=gtts.gTTS("the current location")
             a.save("tts.mp3")
             subprocess.call("mpv tts.mp3",shell=True)
             subprocess.call(msg,shell=True),
+            
         case "sl":
             a=gtts.gTTS("sl command is running")
             a.save("tts.mp3")
@@ -44,7 +57,7 @@ def repeat():
     if msg!="exit":
         repeat()
     else:
-        print("Exit tts .....")
+        print(bcolors.OKGREEN+"Exit tts .....")
         msg1="closing the terminal assistant programe..."
         b=gtts.gTTS(msg1)
         b.save("tts.mp3")
