0d5ff5b440f7235bc5e193b1a972f7586602cebd
blackdevil <blackdevil@kali>
2024-08-24 18:32:29 +0530

    version 1.01

--- tts1.py
+++ tts1.py
@@ -1,23 +1,51 @@
 #import modules
 import subprocess
 import gtts
+import time
 #welcome tts msg
-welcomemsg="welcome to text to speech termux"
+welcomemsg="welcome to terminal voice assistant"
 a1=gtts.gTTS(welcomemsg)
-a1.save("tts.mp3")
-subprocess.call("mpv tts.mp3",shell=True)
+a1.save("tts1.mp3")
+subprocess.call("mpv tts1.mp3",shell=True)
 print("welcome to text to speech termux...")
 
 def repeat():
-    msg=input("Enter A Message: ")
-    a=gtts.gTTS(msg)
-    a.save("tts.mp3")
-    subprocess.call("mpv tts.mp3",shell=True)
+    msg=input("Enter A Command: ")
+    
+
+    
+    match msg:
+        case "clear":
+            a=gtts.gTTS("clearing a terminal")
+            a.save("tts.mp3")
+            subprocess.call("mpv tts.mp3",shell=True)
+            subprocess.call(msg,shell=True),
+        case "ls":
+            a=gtts.gTTS("listing a files")
+            a.save("tts.mp3")
+            subprocess.call("mpv tts.mp3",shell=True)
+            subprocess.call(msg,shell=True),
+        case "pwd":
+            a=gtts.gTTS("the current location")
+            a.save("tts.mp3")
+            subprocess.call("mpv tts.mp3",shell=True)
+            subprocess.call(msg,shell=True),
+        case "sl":
+            a=gtts.gTTS("sl command is running")
+            a.save("tts.mp3")
+            subprocess.call("mpv tts.mp3",shell=True)
+            subprocess.call(msg,shell=True),
+        case _:
+            if msg!="exit":
+                a=gtts.gTTS("your command "+msg+" is executing")
+                a.save("tts.mp3")
+                subprocess.call("mpv tts.mp3",shell=True)
+    
     if msg!="exit":
         repeat()
     else:
         print("Exit tts .....")
-        msg1="closing the text to speech programe..."
+        msg1="closing the terminal assistant programe..."
         b=gtts.gTTS(msg1)
         b.save("tts.mp3")
         subprocess.call("mpv tts.mp3",shell=True)
